b99e6e1a9beae78423bc9054a7786a28122ea05d
robkle <57751018+robkle@users.noreply.github.com>
2019-11-18 18:05:57 +0200

    Add files via upload

---
+++ ft_memmove.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rklein <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/19 17:13:20 by rklein            #+#    #+#             */
+/*   Updated: 2019/10/30 17:46:41 by rklein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
+
+	d = (unsigned char*)dst;
+	s = (unsigned char*)src;
+	i = 0;
+	if (d < s)
+		ft_memcpy(dst, src, len);
+	else
+	{
+		while (len > 0)
+		{
+			d[len - 1] = s[len - 1];
+			len--;
+		}
+	}
+	return (dst);
+}
